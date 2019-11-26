97225bfc82cb240a10208da9279b9c7482cd1592
jperlas2019 <55457954+jperlas2019@users.noreply.github.com>
2019-11-26 10:45:42 -0800

    Update hangman.js

--- js/hangman.js
+++ js/hangman.js
@@ -1,20 +1,10 @@
 "use strict";
 let words = ["COMMITTEE", "CIVILIZED", "COMMUNITY", "GRAPEFRUIT", "GRIFFIN", "ORANGUTAN", "TECHNIQUE", "UNCHARTED", "ELIMINATE", "LAMBORGHINI"];
-let description = ["A group of people appointed for a specific function, typically consisting of members of a larger group.",
-    "Polite and well-mannered.",
-    "a feeling of fellowship with others, as a result of sharing common attitudes, interests, and goals.",
-    "A large round yellow citrus fruit with an acid juicy pulp.",
-    "A mythical creature with the head and wings of an eagle and the body of a lion",
-    "Red-haired apes that live in the tropical rainforests of Sumatra and Borneo",
-    "A way of carrying out a particular task",
-    "An area of land or sea that is not mapped or surveyed.",
-    "Completely remove or get rid of (something).",
-    "Amir's favorite car brand"];
 let score = 0;
-let guesses = 0;
 let ranks = [];
-let displayed, word, desc;
-let selector = randomGen(0, 10);
+let word;
+let displayed;
+let guesses = 0;
 
 //TODO Display the letters within the word when guessed correctly
 // Clear previous buttons if game resets
@@ -30,7 +20,7 @@ function Button(letter){ //BUTTON OBJECT CONSTRUCTOR -JAGUAR
     this.button.innerHTML = letter
     this.button.onclick = checkIn(letter, word, this.button)
     this.button.className = "button"
-    document.getElementById("buttons").appendChild(this.button)
+    document.body.appendChild(this.button)
 }
 
 function alphaButton(){
@@ -52,45 +42,65 @@ function checkIn(letter, myword, btn){
             }
         }
         if(check){
+            alert("Correct!"); //Testing only
+            //document.getElementById("image").src = "img/nice.jpg";
+            //document.getElementById("image").style.display = "inline-block";
         } else {
             score--;
             guesses += 1;
-            hangmanDisplay(guesses);
             document.getElementById("guesses").innerHTML = "LIVES: " + (7 - guesses);
             document.getElementById("score").innerHTML = "SCORE: " + score;
+            alert("Wrong"); //Testing only
+            //document.getElementById("image").src = "img/wrong.jpg";
+            //document.getElementById("image").style.display = "inline-block";
         }
         btn.style.display = "none";
-        displayWord(displayed, desc);
+        displayLetter(displayed, letter, word);
         if(guesses == 7){
+
             initialize();
         }
     }
 }
 
-//Generates a new word
-function newWord(){
-    let myword = words[selector];
-    return myword;
-}
-
-//Generates a description
-function newDescription(){
-    let mydescription = description[selector];
-    return mydescription
-}
-
-// displays word with spaces between each letter
-function displayWord(myword, desc){
+// Replace blanks with correct letter
+function displayLetter(myword, letter, word){
     let display = "";
+    let mywordarr = myword.split("")
     for(let i = 0; i < myword.length; i++){
-        if(i != myword.length - 1){
-            display += myword.charAt(i) + " ";
+        if(i != myword.length){
+            if(letter === word.charAt(i)){
+                display = display + letter
+                //myword = myword.charAt(i) + letter
+                mywordarr[i] = letter
+            } else {
+                display += myword.charAt(i) + " ";
+            }
         } else {
             display += myword.charAt(i);
         }
     }
+    displayed = mywordarr.join("")
     document.getElementById("display").innerHTML = display;
-    document.getElementById("desc").innerHTML = desc;
+}
+
+//Generates a new word
+function newWord(){
+    let myword = words[randomGen(0, 10)];
+    return myword;
+}
+
+// displays word with spaces between each letter
+function displayWord(myword){
+    let display = "";
+    for(let i = 0; i < myword.length; i++){
+        if(i != myword.length - 1){
+            display += myword.charAt(i) + " ";
+            } else {
+            display += myword.charAt(i);
+        }
+    }
+    document.getElementById("display").innerHTML = display;
 }
 
 // Converts each letter in word to an _
@@ -105,28 +115,14 @@ function hideWord(myword){
 //initialize game
 function initialize(){
     word = newWord();
-    desc = newDescription();
     displayed = hideWord(word);
-    displayWord(displayed, desc);
+    displayWord(displayed);
     document.getElementById("buttons").innerHTML = "";
     document.getElementById("guesses").innerHTML = "LIVES: 7";
     document.getElementById("score").innerHTML = "SCORE: 0";
     alphaButton();
     score = 0;
     guesses = 0;
-    hangmanDisplay(guesses);
-    selector = randomGen(0, 10);
-    resetGame();
-}
-
-function resetGame(){
-    let resetButton = document.getElementById('resetButton');
-    resetButton.onclick = initialize;
-}
-
-function hangmanDisplay(level){
-    let hangman = document.getElementById('hangman_display');
-    hangman.src = "img/h" + level + ".jpg"
 }
 
 //TODO Check if word is complete! (possibly check if each character is now a character)
